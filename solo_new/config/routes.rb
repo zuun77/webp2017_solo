1c8a89f75ba0a51ff02f8b767c585d0644db34d3
zuun <year2002777@gmail.com>
2017-06-03 00:56:57 +0900

    minor bugs fixed

--- solo_new/config/routes.rb
+++ solo_new/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :horoscopes
+  get '/' => "horoscopes#index"
   get '/api/horoscopes/fortune/:id' => 'horoscopes#fortune'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
